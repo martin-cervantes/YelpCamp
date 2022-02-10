806c94a1058fedf7274c6698699ce6055be291b7
Martin Cervantes <cervantes.martine@gmail.com>
2022-02-09 18:32:19 -0700

    Add routing to basic express app

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 
 const port_num = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  console.log('get root');
+  res.send('<h1>Get Root</h1>');
+});
+
+app.get('/cats', (req, res) => {
+  console.log('get cats');
+  res.send('<h1>Get Cats</h1>');
+});
+
+app.get('/dogs', (req, res) => {
+  console.log('get dogs');
+  res.send('<h1>Get Dogs</h1>');
+});
+
 app.listen(port_num, () => {
   console.log(`Listening on port ${port_num}`);
 });
